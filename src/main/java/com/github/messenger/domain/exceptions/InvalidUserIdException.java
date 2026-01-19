c5cec3b0bbb3d3f481dbe5bc3da63c05462d973c
afority <pukceji@gmail.com>
2026-01-19 22:07:32 +0500

    Удалил не нужно исключение

--- src/main/java/com/github/messenger/domain/exceptions/InvalidUserIdException.java
+++
@@ -1,7 +0,0 @@
-package com.github.messenger.domain.exceptions;
-
-public class InvalidUserIdException extends RuntimeException {
-    public InvalidUserIdException() {
-        super("Invalid userId");
-    }
-}
