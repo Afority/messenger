36444146b9cdcd78000b6ca21cadadab9a92354a
afority <pukceji@gmail.com>
2026-01-20 13:32:33 +0500

    поменял long на UUID

--- src/main/java/com/github/messenger/domain/entity/ChatEntity.java
+++ src/main/java/com/github/messenger/domain/entity/ChatEntity.java
@@ -1,17 +1,17 @@
 package com.github.messenger.domain.entity;
 
-import com.github.messenger.domain.value_objects.ChatId;
 import lombok.Getter;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 @Getter
 public class ChatEntity {
-    private ChatId chatId;
+    private final UUID chatId;
     private List<UserEntity> participants;
 
-    public ChatEntity(ChatId chatId, List<UserEntity> participants) {
+    public ChatEntity(UUID chatId, List<UserEntity> participants) {
         Objects.requireNonNull(chatId, "chatId cannot be null");
         Objects.requireNonNull(participants, "participants cannot be null");
 
