e129bb56b284bb2e6e9350645fb31b0bdb0ad2b4
afority <pukceji@gmail.com>
2026-01-21 22:18:05 +0500

    Убрал entity в названии, упростил геттеры, добавил состояния онлайн и last seen at

--- src/main/java/com/github/messenger/domain/entity/User.java
+++ src/main/java/com/github/messenger/domain/entity/User.java
@@ -4,26 +4,32 @@ import com.github.messenger.domain.value_objects.Login;
 import com.github.messenger.domain.value_objects.UserId;
 import lombok.Getter;
 
+import java.time.Instant;
 import java.util.Objects;
 
-public class UserEntity {
-    @Getter
+@Getter
+public class User {
     private UserId id;
 
-    @Getter
     private Login login;
 
-    @Getter
     // hashed
     private String password;
 
-    public UserEntity(UserId id, Login login, String password) {
+    private boolean isOnline;
+
+    private Instant lastSeen;
+
+    public User(UserId id, Login login, String password, boolean isOnline, Instant lastSeen) {
         Objects.requireNonNull(id, "id cannot be null");
         Objects.requireNonNull(login, "login cannot be null");
         Objects.requireNonNull(password, "password cannot be null");
+        Objects.requireNonNull(lastSeen, "lastSeen cannot be null");
 
         this.id = id;
         this.login = login;
         this.password = password;
+        this.isOnline = isOnline;
+        this.lastSeen = lastSeen;
     }
 }
