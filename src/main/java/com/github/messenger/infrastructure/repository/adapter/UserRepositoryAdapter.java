9b447bfa1ef9a6ac2f75ae6dfd4dde1cef4bde45
afority <pukceji@gmail.com>
2026-01-21 22:24:05 +0500

    После переделки domain пофиксил адаптер

--- src/main/java/com/github/messenger/infrastructure/repository/adapter/UserRepositoryAdapter.java
+++ src/main/java/com/github/messenger/infrastructure/repository/adapter/UserRepositoryAdapter.java
@@ -1,12 +1,13 @@
 package com.github.messenger.infrastructure.repository.adapter;
 
-import com.github.messenger.domain.entity.UserEntity;
+import com.github.messenger.domain.entity.User;
 import com.github.messenger.domain.repository.UserRepository;
 import com.github.messenger.domain.value_objects.Login;
 import com.github.messenger.domain.value_objects.UserId;
 import com.github.messenger.infrastructure.mapper.UserMapper;
 import com.github.messenger.infrastructure.repository.UserJpaRepository;
-import com.github.messenger.infrastructure.repository.entity.UserDbEntity;
+import com.github.messenger.infrastructure.repository.entity.UserJpaEntity;
+import com.github.messenger.infrastructure.repository.entity.UserPresenceJpaEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,14 +25,14 @@ public class UserRepositoryAdapter implements UserRepository {
     }
 
     @Override
-    public Optional<UserEntity> findById(UserId id) {
+    public Optional<User> findById(UserId id) {
         return userJpaRepository
-                .findById(id.getValue())
+                .findById(id.value())
                 .map(dbEntity -> userMapper.mapToEntity(dbEntity));
     }
 
     @Override
-    public Optional<UserEntity> findByLogin(Login login) {
+    public Optional<User> findByLogin(Login login) {
         return userJpaRepository
                 .findByLogin(login.getValue())
                 .map(dbEntity -> userMapper.mapToEntity(dbEntity));
@@ -39,7 +40,7 @@ public class UserRepositoryAdapter implements UserRepository {
 
     @Override
     public void save(Login login, String hashedPassword) {
-        userJpaRepository.save(new UserDbEntity(login.getValue(), hashedPassword));
+        userJpaRepository.save(new UserJpaEntity(login.getValue(), hashedPassword, new UserPresenceJpaEntity()));
     }
 
     @Override
